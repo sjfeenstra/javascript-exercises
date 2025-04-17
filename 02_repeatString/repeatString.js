e5d9940b17f34fba6ed707a755a51c6bac838fc4
sjfeenstra <sjoerdfeenstra_7@hotmail.com>
2025-04-17 15:02:57 +0200

    Completed exercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0){
+        return "ERROR"
+    }
+    answer = ''
+    for(i=0; i<num; i++){
+        answer += string;
+    }
+    return answer;
 };
 
 // Do not edit below this line
