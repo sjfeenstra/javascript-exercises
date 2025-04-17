fafceb0917fba7c5813993d18e65baa5267f0373
sjfeenstra <sjoerdfeenstra_7@hotmail.com>
2025-04-17 15:45:40 +0200

    Completed exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...values) {
+    for (let value of values) {
+        while(array.includes(value)){
+            array.splice(array.indexOf(value),1)
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
