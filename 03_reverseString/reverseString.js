910d13d53c057e524a4ec9568da9e479b7d84f19
sjfeenstra <sjoerdfeenstra_7@hotmail.com>
2025-04-17 15:20:30 +0200

    Completed exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = []
+    stringArray = string.split("")
+    return stringArray.reverse().join("");
 };
 
 // Do not edit below this line
