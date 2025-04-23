9cacbc05f68b772bd664670bf77a74bd206400e8
sjfeenstra <sjoerdfeenstra_7@hotmail.com>
2025-04-23 21:56:15 +0200

    Completed array method exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if (index < 0) {
+        return "OOPS"
+    }
+    arr = [0,1,1]
+    for (let i = 2; i <= index; i++) {
+        arr.push((arr[i]+arr[i-1]))
+    }
+    console.log(arr)
+    return arr[index]
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,7 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.forEach((item, i) => { if (item.yearOfDeath == undefined) people[i].yearOfDeath = 2025; });
+    sorted = [...people].sort((a,b) => (a.yearOfDeath-a.yearOfBirth)-(b.yearOfDeath-b.yearOfBirth))
+    return sorted[sorted.length-1]
 };
 
 // Do not edit below this line
